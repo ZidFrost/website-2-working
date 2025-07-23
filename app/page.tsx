dc0a55cec145a77dac8c26f62bc54ff01b7da1e3
ZidFrost <74118921+ZidFrost@users.noreply.github.com>
2025-07-23 23:05:31 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-app/globals.css
-app/layout.tsx
-app/layout.tsx
 export default function Home() {
   return (
     <main className="text-center p-10">
       <h1 className="text-4xl font-bold mb-4">Velkommen til $HDY</h1>
       <p className="text-lg">The future of Norwegian memecoins. 🚀</p>
     </main>
- 
+  );
+}
