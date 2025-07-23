68c41a3998ad52fb192f9e6007c238c49937c17c
ZidFrost <74118921+ZidFrost@users.noreply.github.com>
2025-07-23 22:47:36 +0200

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-4xl font-bold mb-4">Hello from $HDY</h1>
+      <p className="text-lg">This is the start of your memecoin site!</p>
+    </main>
+  );
+}
